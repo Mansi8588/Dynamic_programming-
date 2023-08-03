08e83e9998c71ea95cdc62161ac1399fa1a908a7
Mansi Rai <139311034+Mansi8588@users.noreply.github.com>
2023-08-03 18:39:49 +0530

    Update fibonacci.java

--- fibonacci.java
+++ fibonacci.java
@@ -1,1 +1,12 @@
-
+public class fibonacci {
+  public static int fib(int data) {
+  if(data==1||data==0)
+  return data;
+   return fib(data-1)+fib(data-2);
+  }
+    public static void main(String[] arg) {
+      int n=5;
+      System.out.println(fib(n));
+    }
+  }
+      
